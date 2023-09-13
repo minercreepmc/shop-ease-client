15aca11e3fda552766a9609aded874dd4b72d48c
tinpham <heoconlinhat123@gmail.com>
2023-09-13 14:12:38 +0700

    add stuff

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '@shared/services';
 import {  map, catchError, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(
     private readonly authService: AuthService,
     private router: Router
